5f0059529236d0b5ca24a9dee3d22b8a132b42f6
Oncu <oohancan@gmail.com>
2024-05-30 22:55:02 +0300

    Initial Commit

---
+++ FlipClockAnimation/FlipClockAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClockAnimationApp.swift
+//  FlipClockAnimation
+//
+//  Created by Oncu Can on 30.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlipClockAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
