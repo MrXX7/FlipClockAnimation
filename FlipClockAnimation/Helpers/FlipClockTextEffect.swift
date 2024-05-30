1bf70229b1a681c1fb54bad9cc033cdd693b5810
Oncu <oohancan@gmail.com>
2024-05-30 22:58:58 +0300

    added FlipClockTextEffect

---
+++ FlipClockAnimation/Helpers/FlipClockTextEffect.swift
@@ -0,0 +1,18 @@
+//
+//  FlipClockTextEffect.swift
+//  FlipClockAnimation
+//
+//  Created by Oncu Can on 30.05.2024.
+//
+
+import SwiftUI
+
+struct FlipClockTextEffect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlipClockTextEffect()
+}
